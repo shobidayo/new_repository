82b709d25db00068a113ef5c3e05584b72f6fa08
shobidayo <aobasho8@gmail.com>
2023-08-31 21:53:54 +0900

    ブログ投稿一覧画面の表示と

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1> Blog Name</h1>
+        <div class = "posts"> <!--Postcontroller.phpでwith（）変数名にpostsを設定したのでここで使える-->
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                    <h3 class ="title">{{$post->title}}</h3>
+                    <p class = "body">{{$post->body}}</p>
+                    <h4 class = "title">{{$post->title}}</h4>
+                    <p class = "body">{{$post ->body}}</p>
+                </div>
+            @endforeach
+        </div>
+    </body>
+</html>
