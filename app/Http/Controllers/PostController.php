2faeeb91bb772b6f18ad5dbc77d515278e826907
shobidayo <aobasho8@gmail.com>
2023-09-02 11:14:54 +0900

    ブログ投稿編集画面を作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,9 +27,17 @@ class Postcontroller extends Controller
     {
         $input = $request['post'];
         $post -> fill($input) -> save();
-        
-        return redirect('/posts/'.$post ->id);
-        
+        return redirect('/posts/'.$post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('posts.edit') ->with(['post'=> $post]);
+    }
+    public function update(PostRequest $request , Post $post)
+    {
+        $input_post = $request['post'];
+        $post -> fill($input_post) -> save();
+        return redirect('/posts/'.$post->id);
     }
 }
 ?>
